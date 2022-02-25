9d5493f48e753324cf59ade2b64d17cbcb2e19de
gallasglasses <gallasglasses@gmail.com>
2022-02-25 23:21:58 +0300

    create project

---
+++ JamProject/Source/JamProject/JamProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JamProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JAMPROJECT_API AJamProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
